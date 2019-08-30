88545c74f6d45f67f9d032e7c775117aa0d3bd03
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-08-30 11:31:10 +0600

    First commit.

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\Google Drive\4-1 (R)\CSE 408 Software Development\BackendofBarivara\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
