d48ad9a0a01176f3d9ecb30b85cf0babfe885d9a
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-09-05 12:03:53 +0600

    Modified model Song.

--- BackendofBarivara/settings.py
+++ BackendofBarivara/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#77(&=-^kq6=#i)61dk$*@#o3td1zf&id^-41+@+6x=r0+hqqf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True # TODO Change this to False before production
 
-ALLOWED_HOSTS = ['192.168.0.108']
+ALLOWED_HOSTS = ['192.168.0.106']
 
 
 # Application definition
