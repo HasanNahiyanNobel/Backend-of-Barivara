5b1fba8a9bc3a0f4b9539b65f7e785b6fe986fd3
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-09-08 16:12:15 +0600

    Changed admin panel display to table.

--- houses/admin.py
+++ houses/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from houses.models import House, User
 
-admin.site.register(House)
-admin.site.register(User)
+
+class HouseAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in House._meta.get_fields()]
+
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in User._meta.get_fields()]
+
+
+admin.site.register(House, HouseAdmin)
+admin.site.register(User, UserAdmin)
