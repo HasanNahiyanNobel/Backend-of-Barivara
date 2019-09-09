971e824d5104f7b83863748f6025ba2550826c0e
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2019-09-09 15:39:22 +0600

    Added pagination.

--- BackendofBarivara/settings.py
+++ BackendofBarivara/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -23,10 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#77(&=-^kq6=#i)61dk$*@#o3td1zf&id^-41+@+6x=r0+hqqf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # TODO Change this to False before production
+DEBUG = True  # TODO Change this to False before production
 
-ALLOWED_HOSTS = ['192.168.0.106', '192.168.0.108']
-
+ALLOWED_HOSTS = ['192.168.0.106', '192.168.0.108', '172.20.57.21', '192.168.43.127']
 
 # Application definition
 
@@ -41,6 +39,11 @@ INSTALLED_APPS = [
 	'django.contrib.staticfiles',
 	'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+	'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
@@ -73,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BackendofBarivara.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -83,7 +85,6 @@ DATABASES = {
 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -103,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
 	},
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -116,7 +116,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
